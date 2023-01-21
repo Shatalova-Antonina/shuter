b1f7f590ac2adaa00ef2474aba68109e84bcb544
Shatalova Tonya <tonechka.shatalova@gmail.com>
2023-01-21 11:07:02 +0200

    add background image

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame
+import os
 pygame.init()
+
+def file_path(file_name):
+    folder_path = os.path.abspath(__file__ + "/..")
+    path = os.path.join(folder_path, file_name)
+    return path
 
 FPS = 40
 WIN_WIDTH = 700
@@ -8,6 +14,9 @@ WIN_HEIGHT = 500
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
 
+background = pygame.image.load(file_path("fon_shooter.jpg"))
+background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGHT))
+
 play = True
 game = True
 
@@ -15,5 +24,9 @@ while game == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+
+    if play == True:
+        window.blit(background, (0, 0))
+
     clock.tick(FPS)
     pygame.display.update()
