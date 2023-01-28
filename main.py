136be4f61d02ddee50f58cf7f303c4ac741d5693
Shatalova Tonya <tonechka.shatalova@gmail.com>
2023-01-28 10:17:31 +0200

    added class GameSprite

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ clock = pygame.time.Clock()
 background = pygame.image.load(file_path("fon_shooter.jpg"))
 background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGHT))
 
+class GameSprite(pygame.sprite.Sprite):
+    def __init__ (self, image, x, y, width, height, speed):
+        super().__init__()
+        self.image = pygame.image.load(file_path(image))
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect()
+        self.recr.x = x
+        self.rect.y = y
+        self.speed = speed
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 pygame.mixer.music.load("shooter_music.ogg")
 pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(-1)
